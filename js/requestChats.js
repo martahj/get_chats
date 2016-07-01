05978664268582df58d365a851b6aa39c46ef0c0
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-07-01 16:38:32 -0500

    getting data from server how we want

--- js/requestChats.js
+++ js/requestChats.js
@@ -1,16 +1,42 @@
+"use strict"
+
 const fetch = require('isomorphic-fetch');
 const Promise = require('bluebird');
-
-const server = 'https://api.parse.com/1/classes/chatterbox/';
+const config = require('../config.js');
+const url = 'https://api.parse.com/1/classes/chatterbox';
 
 const requestChats = {};
 module.exports = requestChats;
 
-requestChats.getRound = (skip) => {
-	//
+requestChats.getRound = (skip, order, limit) => {
+	let skipUrl = 'skip=' + (skip || 0);
+	let orderUrl = 'order=' + (order || '-createdAt');
+	let limitUrl = 'limit=' + (limit || 100);
+
+	let u = url + '?' + skipUrl + '&' + orderUrl + '&' + limitUrl;
+	return fetch(u, {
+		headers: {
+			'X-Parse-Application-Id': config.id,
+			'X-Parse-REST-API-Key': config.key,
+			'contentType': 'application/json'
+			// 'data': { order: 'createdAt'}
+			// 'skip': skip,
+			// 'order':  '-createdAt'
+		}
+	})
+	.then( result => result.json() )
+	.catch( err => {
+		console.log('noooo error', err);
+		throw err;
+	})
 }
 
+//there are less than 1000 chats so this will work
 requestChats.getAll = () => {
-	//
+	return requestChats.getRound(0, '-createdAt', 2000)
+	.then( results => results.results)
+	.catch( err => {
+		console.log('error getting all', err)
+		return err;
+	})
 }
-
--- test/requests_test.js
+++ test/requests_test.js
@@ -1,21 +1,75 @@
+"use strict"
+
 const expect = require('chai').expect;
-const requestChats = require('../requestChats')
+const _ = require('lodash');
+const requestChats = require('../js/requestChats.js')
 
 describe('** requesting chats **', function() {
 
-	it('returns chats', function() {
-		//
-	})
+	let skipZero;
 
-	it('returns the same chats if the same skip is provided', function() {
-		//
+	it('returns chats', function() {
+		return requestChats.getRound(0)
+		.then( result => {
+			expect(result).to.be.an('object');
+
+			expect(result.results).to.be.an('array');
+			expect(result.results).to.have.length(100);
+			skipZero = result.results;
+
+			expect(result.results[0]).to.be.an('object');
+			expect(result.results[0]).to.have.any.keys('createdAt', 'objectId', 'roomname', 'text', 'opponents', 'updatedAt', 'username')
+		})
 	})
 
 	it('returns different chats if a different skip is provided', function() {
-		//
+		return requestChats.getRound(100)
+		.then( result => {
+			expect(result).to.be.an('object');
+
+			expect(result.results).to.be.an('array');
+			expect(result.results).to.have.length(100);
+
+			expect(objectsMatch(result.results, skipZero)).to.be.false; //should be false
+		})
 	})
 
-	it('gets all the chats and packages them as one object', function() {
-		//
+	it('returns the same chats if the same skip is provided', function() {
+		return requestChats.getRound(0)
+		.then( result => {
+			expect(result).to.be.an('object');
+
+			expect(result.results).to.be.an('array');
+			expect(result.results).to.have.length(100);
+
+			expect(objectsMatch(result.results, skipZero)).to.be.true; 
+		})
+	})
+
+	it('takes an optional order', function() {
+		return requestChats.getRound(0, 'createdAt')
+		.then( result => {
+			expect(result).to.be.an('object');
+
+			expect(result.results).to.be.an('array');
+			expect(result.results).to.have.length(100);
+
+			expect(objectsMatch(result.results, skipZero)).to.be.false;
+		})
+	})
+
+	it('gets all the chats and packages them as one array', function() {
+		return requestChats.getAll()
+		.then( result => {
+			expect(result).to.be.an('array');
+			expect(result.length > 100).to.be.true;
+		})
 	})
 })
+
+function objectsMatch(a, b) {
+	return _.isEqual(a, b);
+}
+
+
+
