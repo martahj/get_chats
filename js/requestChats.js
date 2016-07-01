e6018cd7d694d252a8aa60d700ac41cbfe88f56e
Marta Johnson <marta.hourigan.johnson@gmail.com>
2016-07-01 11:57:45 -0500

    first commit

---
+++ js/requestChats.js
@@ -0,0 +1,16 @@
+const fetch = require('isomorphic-fetch');
+const Promise = require('bluebird');
+
+const server = 'https://api.parse.com/1/classes/chatterbox/';
+
+const requestChats = {};
+module.exports = requestChats;
+
+requestChats.getRound = (skip) => {
+	//
+}
+
+requestChats.getAll = () => {
+	//
+}
+
---
+++ test/requests_test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+const requestChats = require('../requestChats')
+
+describe('** requesting chats **', function() {
+
+	it('returns chats', function() {
+		//
+	})
+
+	it('returns the same chats if the same skip is provided', function() {
+		//
+	})
+
+	it('returns different chats if a different skip is provided', function() {
+		//
+	})
+
+	it('gets all the chats and packages them as one object', function() {
+		//
+	})
+})
